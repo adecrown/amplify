140425304f7e36adc08264c935c13c2c7aef77cb
Adekunle Ogunade <adecrown1@outlook.com>
2023-05-22 11:58:56 +0100

    another. build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learning about Next.js in an interactive course with&nbsp;quizzes more Ade!
+            Learning about Next.js in an interacting very huge course with&nbsp;quizzes more Ade!
           </p>
         </a>
 
